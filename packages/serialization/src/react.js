ecc166bb3b6444c6fa50c60acf87205d72de456f
Vitaliy Akimov <vitaliy.akimov@gmail.com>
2018-05-20 22:16:23 +0300

    react elements serialzation

---
+++ packages/serialization/src/react.js
@@ -0,0 +1,30 @@
+import * as R from "./main"
+
+/** registers serialization format descriptor for React elements */
+export default function register(React) {
+  R.regMeta({
+    name: "ReactElement",
+    typeofTag: Symbol.for("react.element"),
+    write(ctx, value) {
+      const props = {children:void 0}
+      if (value.key != null)
+        props.key = value.key
+      return {
+        "#type": this.name,
+        "#data": ctx.step({props:{...value.props,...props},
+                           children:React.Children.toArray(value.props.children),
+                           type:value.type})
+      }
+    },
+    readContent() {},
+    read(ctx, json) {
+      const data = ctx.step(json["#data"])
+      const children = data.children || []
+      return React.createElement(data.type, data.props, ...children)
+    },
+    create(ctx, json) {
+      return this.read(ctx, json)
+    }
+  })
+}
+
