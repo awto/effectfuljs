ef3bb8a2a9669f1437e493150ab8de7bb19aaa0f
Vitaliy Akimov <vitaliy.akimov@gmail.com>
2019-02-24 20:33:30 +0200

    defunct preset

---
+++ packages/core/src/presets/defunct.js
@@ -0,0 +1,15 @@
+export default {
+  defunct: true,
+  defunctStateDiscriminant:"arg",
+  storeRunningCont: "$cur",
+  scopeContext: true,
+  scopePrefix: true,
+  static: true,
+  contextMethodOps: true,
+  inlineRaiseOp: "throw",
+  inlineJsExceptions: true,
+  inlinePureJumps: "tail",
+  keepLastPure:true,
+  keepLastRaise:true,
+  par:true
+}
