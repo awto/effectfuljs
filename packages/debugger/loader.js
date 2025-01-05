6d7cea24957df45359832a2a0e47c8e6db788639
Vitalii Akimov <vitaliy.akimov@gmail.com>
2025-01-05 20:36:56 +0000

    loader's fixes

--- packages/debugger/loader.js
+++ packages/debugger/loader.js
@@ -1,11 +1,17 @@
 const compile = require("./config/node/compile");
-const config = require("./deriveConfig");
+const config = require("./config/deriveConfig");
+const path = require("path");
 
 module.exports = function (source) {
+  if (typeof source !== "string" || !/\.[cme]?[tj]sx?$/.test(path.extname(this.resourcePath)))
+    return source;
   const savedLoaderPrefix = config.loaderPrefix;
   try {
     config.loaderPrefix = "@effectful/debugger/loader!";
     return compile(source, this.resourcePath, this._module);
+  } catch (e) {
+    console.error(`couldn't transpile ${this.resourcePath}`, e);
+    return source;
   } finally {
     config.loaderPrefix = savedLoaderPrefix;
   }
